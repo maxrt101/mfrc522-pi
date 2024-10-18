868ec4af591a30a5966707c0be1a9cf01186443f
Maksym Tkachuk <tkachuk.m@ajax.systems>
2024-10-18 16:06:48 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mfrc522pi",
+    version="0.1.0",
+    author="maxrtx",
+    author_email="maxrt101@git",
+    description="Setup for mfrc522pi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maxrt101/mfrc522-pi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.9',
+)
