0122102ff35f77fc6691e9a8699f5f280c64a80d
Maksym Tkachuk <tkachuk.m@ajax.systems>
2024-10-18 23:10:06 +0300

    Refactor branching of examples

--- examples/read.py
+++ examples/read.py
@@ -16,30 +16,40 @@ def main():
         while True:
             res = reader.request(PICC.REQIDL)
 
-            if res.status == Status.OK:
-                print('\nDetected a card')
+            if res.status != Status.OK:
+                continue
+
+            print('\nDetected a card')
 
             res = reader.anti_collision()
 
+            if res.status != Status.OK:
+                print(f'AntiCollision error: {res.status}')
+                continue
+
+            print(f'UID: {" ".join([f"0x{x:02X}" for x in res.uid])}')
+
+            key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
+            status = reader.select_tag(res.uid)
+
+            if status != Status.OK:
+                print(f'Selection error: {res.status}')
+                continue
+
+            status = reader.authenticate(PICC.AUTHENT1A, 8, key, res.uid)
+
+            if status != Status.OK:
+                print(f'Authentication error: {res.status}')
+                continue
+
+            print(f'Reading block {BLOCK}')
+            res = reader.read_block(BLOCK)
+            reader.stop_crypto1()
             if res.status == Status.OK:
-                print(f'UID: {" ".join([f"0x{x:02X}" for x in res.uid])}')
-
-                key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
-
-                reader.select_tag(res.uid)
-
-                status = reader.authenticate(PICC.AUTHENT1A, 8, key, res.uid)
-
-                if status == Status.OK:
-                    print(f'Reading block {BLOCK}')
-                    res = reader.read_block(BLOCK)
-                    reader.stop_crypto1()
-                    if res.status == Status.OK:
-                        print(' '.join(f'0x{x:02X}' for x in res.data))
-                    else:
-                        print(f'Error reading block {BLOCK}: {res.status}')
-                else:
-                    print(f'Authentication error: {res.status}')
+                print(' '.join(f'0x{x:02X}' for x in res.data))
+            else:
+                print(f'Error reading block {BLOCK}: {res.status}')
     except KeyboardInterrupt:
         print('Exiting...')
     except Exception as e:
